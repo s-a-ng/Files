107a5cef00c7856109683ff1a2821446ddec5ada
sang <61368081+sangwriter@users.noreply.github.com>
2022-12-27 20:50:35 -0800

    Update 5.1 bytecode decoder.lua

--- 5.1 bytecode decoder.lua
+++ 5.1 bytecode decoder.lua
@@ -1,234 +1,484 @@
---- ripped from lbi
+-- ripped from fione
+local bit = bit or bit32 or require('bit')
+local stm_lua_func
 
+-- SETLIST config
 
-local lua_opcode_types = {
-	"ABC",  "ABx", "ABC",  "ABC",
-	"ABC",  "ABx", "ABC",  "ABx",
-	"ABC",  "ABC", "ABC",  "ABC",
-	"ABC",  "ABC", "ABC",  "ABC",
-	"ABC",  "ABC", "ABC",  "ABC",
-	"ABC",  "ABC", "AsBx", "ABC",
-	"ABC",  "ABC", "ABC",  "ABC",
-	"ABC",  "ABC", "ABC",  "AsBx",
-	"AsBx", "ABC", "ABC", "ABC",
-	"ABx",  "ABC",
+-- remap for better lookup
+local OPCODE_RM = {
+	-- level 1
+	[22] = 18, -- JMP
+	[31] = 8, -- FORLOOP
+	[33] = 28, -- TFORLOOP
+	-- level 2
+	[0] = 3, -- MOVE
+	[1] = 13, -- LOADK
+	[2] = 23, -- LOADBOOL
+	[26] = 33, -- TEST
+	-- level 3
+	[12] = 1, -- ADD
+	[13] = 6, -- SUB
+	[14] = 10, -- MUL
+	[15] = 16, -- DIV
+	[16] = 20, -- MOD
+	[17] = 26, -- POW
+	[18] = 30, -- UNM
+	[19] = 36, -- NOT
+	-- level 4
+	[3] = 0, -- LOADNIL
+	[4] = 2, -- GETUPVAL
+	[5] = 4, -- GETGLOBAL
+	[6] = 7, -- GETTABLE
+	[7] = 9, -- SETGLOBAL
+	[8] = 12, -- SETUPVAL
+	[9] = 14, -- SETTABLE
+	[10] = 17, -- NEWTABLE
+	[20] = 19, -- LEN
+	[21] = 22, -- CONCAT
+	[23] = 24, -- EQ
+	[24] = 27, -- LT
+	[25] = 29, -- LE
+	[27] = 32, -- TESTSET
+	[32] = 34, -- FORPREP
+	[34] = 37, -- SETLIST
+	-- level 5
+	[11] = 5, -- SELF
+	[28] = 11, -- CALL
+	[29] = 15, -- TAILCALL
+	[30] = 21, -- RETURN
+	[35] = 25, -- CLOSE
+	[36] = 31, -- CLOSURE
+	[37] = 35, -- VARARG
 }
 
+-- opcode types for getting values
+local OPCODE_T = {
+	[0] = 'ABC',
+	'ABx',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABx',
+	'ABC',
+	'ABx',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'AsBx',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'AsBx',
+	'AsBx',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABx',
+	'ABC',
+}
 
---- Extract bits from an integer
---@author: Stravant
-local function get_bits(input, n, n2)
-	if n2 then
-		local total = 0
-		local digitn = 0
-		for i = n, n2 do
-			total = total + 2^digitn*get_bits(input, i)
-			digitn = digitn + 1
+local OPCODE_M = {
+	[0] = {b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgK', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgN'},
+	{b = 'OpArgK', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgN'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgR', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgR'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgR', c = 'OpArgU'},
+	{b = 'OpArgR', c = 'OpArgU'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgU', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgN', c = 'OpArgU'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgN', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgN'},
+}
+
+-- int rd_int_basic(string src, int s, int e, int d)
+-- @src - Source binary string
+-- @s - Start index of a little endian integer
+-- @e - End index of the integer
+-- @d - Direction of the loop
+local function rd_int_basic(src, s, e, d)
+	local num = 0
+
+	-- if bb[l] > 127 then -- signed negative
+	-- 	num = num - 256 ^ l
+	-- 	bb[l] = bb[l] - 128
+	-- end
+
+	for i = s, e, d do
+		local mul = 256 ^ math.abs(i - s)
+
+		num = num + mul * string.byte(src, i, i)
+	end
+
+	return num
+end
+
+-- float rd_flt_basic(byte f1..8)
+-- @f1..4 - The 4 bytes composing a little endian float
+local function rd_flt_basic(f1, f2, f3, f4)
+	local sign = (-1) ^ bit.rshift(f4, 7)
+	local exp = bit.rshift(f3, 7) + bit.lshift(bit.band(f4, 0x7F), 1)
+	local frac = f1 + bit.lshift(f2, 8) + bit.lshift(bit.band(f3, 0x7F), 16)
+	local normal = 1
+
+	if exp == 0 then
+		if frac == 0 then
+			return sign * 0
+		else
+			normal = 0
+			exp = 1
 		end
-		return total
-	else
-		local pn = 2^(n-1)
-		return (input % (pn + pn) >= pn) and 1 or 0
+	elseif exp == 0x7F then
+		if frac == 0 then
+			return sign * (1 / 0)
+		else
+			return sign * (0 / 0)
+		end
+	end
+
+	return sign * 2 ^ (exp - 127) * (1 + normal / 2 ^ 23)
+end
+
+-- double rd_dbl_basic(byte f1..8)
+-- @f1..8 - The 8 bytes composing a little endian double
+local function rd_dbl_basic(f1, f2, f3, f4, f5, f6, f7, f8)
+	local sign = (-1) ^ bit.rshift(f8, 7)
+	local exp = bit.lshift(bit.band(f8, 0x7F), 4) + bit.rshift(f7, 4)
+	local frac = bit.band(f7, 0x0F) * 2 ^ 48
+	local normal = 1
+
+	frac = frac + (f6 * 2 ^ 40) + (f5 * 2 ^ 32) + (f4 * 2 ^ 24) + (f3 * 2 ^ 16) + (f2 * 2 ^ 8) + f1 -- help
+
+	if exp == 0 then
+		if frac == 0 then
+			return sign * 0
+		else
+			normal = 0
+			exp = 1
+		end
+	elseif exp == 0x7FF then
+		if frac == 0 then
+			return sign * (1 / 0)
+		else
+			return sign * (0 / 0)
+		end
+	end
+
+	return sign * 2 ^ (exp - 1023) * (normal + frac / 2 ^ 52)
+end
+
+-- int rd_int_le(string src, int s, int e)
+-- @src - Source binary string
+-- @s - Start index of a little endian integer
+-- @e - End index of the integer
+local function rd_int_le(src, s, e) return rd_int_basic(src, s, e - 1, 1) end
+
+-- int rd_int_be(string src, int s, int e)
+-- @src - Source binary string
+-- @s - Start index of a big endian integer
+-- @e - End index of the integer
+local function rd_int_be(src, s, e) return rd_int_basic(src, e - 1, s, -1) end
+
+-- float rd_flt_le(string src, int s)
+-- @src - Source binary string
+-- @s - Start index of little endian float
+local function rd_flt_le(src, s) return rd_flt_basic(string.byte(src, s, s + 3)) end
+
+-- float rd_flt_be(string src, int s)
+-- @src - Source binary string
+-- @s - Start index of big endian float
+local function rd_flt_be(src, s)
+	local f1, f2, f3, f4 = string.byte(src, s, s + 3)
+	return rd_flt_basic(f4, f3, f2, f1)
+end
+
+-- double rd_dbl_le(string src, int s)
+-- @src - Source binary string
+-- @s - Start index of little endian double
+local function rd_dbl_le(src, s) return rd_dbl_basic(string.byte(src, s, s + 7)) end
+
+-- double rd_dbl_be(string src, int s)
+-- @src - Source binary string
+-- @s - Start index of big endian double
+local function rd_dbl_be(src, s)
+	local f1, f2, f3, f4, f5, f6, f7, f8 = string.byte(src, s, s + 7) -- same
+	return rd_dbl_basic(f8, f7, f6, f5, f4, f3, f2, f1)
+end
+
+-- to avoid nested ifs in deserializing
+local float_types = {
+	[4] = {little = rd_flt_le, big = rd_flt_be},
+	[8] = {little = rd_dbl_le, big = rd_dbl_be},
+}
+
+-- byte stm_byte(Stream S)
+-- @S - Stream object to read from
+local function stm_byte(S)
+	local idx = S.index
+	local bt = string.byte(S.source, idx, idx)
+
+	S.index = idx + 1
+	return bt
+end
+
+-- string stm_string(Stream S, int len)
+-- @S - Stream object to read from
+-- @len - Length of string being read
+local function stm_string(S, len)
+	local pos = S.index + len
+	local str = string.sub(S.source, S.index, pos - 1)
+
+	S.index = pos
+	return str
+end
+
+-- string stm_lstring(Stream S)
+-- @S - Stream object to read from
+local function stm_lstring(S)
+	local len = S:s_szt()
+	local str
+
+	if len ~= 0 then str = string.sub(stm_string(S, len), 1, -2) end
+
+	return str
+end
+
+-- fn cst_int_rdr(string src, int len, fn func)
+-- @len - Length of type for reader
+-- @func - Reader callback
+local function cst_int_rdr(len, func)
+	return function(S)
+		local pos = S.index + len
+		local int = func(S.source, S.index, pos)
+		S.index = pos
+
+		return int
 	end
 end
 
-local function decode_bytecode(bytecode)
-	local index = 1
-	local big_endian = false
-    local int_size;
-    local size_t;
+-- fn cst_flt_rdr(string src, int len, fn func)
+-- @len - Length of type for reader
+-- @func - Reader callback
+local function cst_flt_rdr(len, func)
+	return function(S)
+		local flt = func(S.source, S.index)
+		S.index = S.index + len
 
-    -- Actual binary decoding functions. Dependant on the bytecode.
-    local get_int, get_size_t;
+		return flt
+	end
+end
 
-	-- Binary decoding helper functions
-	local get_int8, get_int32, get_int64, get_float64, get_string;
-	do
-		function get_int8()
-			local a = bytecode:byte(index, index);
-			index = index + 1
-			return a
+local function stm_inst_list(S)
+	local len = S:s_int()
+	local list = table.create(len)
+
+	for i = 1, len do
+		local ins = S:s_ins()
+		local op = bit.band(ins, 0x3F)
+		local args = OPCODE_T[op]
+		local mode = OPCODE_M[op]
+		local data = {value = ins, op = OPCODE_RM[op], A = bit.band(bit.rshift(ins, 6), 0xFF)}
+
+		if args == 'ABC' then
+			data.B = bit.band(bit.rshift(ins, 23), 0x1FF)
+			data.C = bit.band(bit.rshift(ins, 14), 0x1FF)
+			data.is_KB = mode.b == 'OpArgK' and data.B > 0xFF -- post process optimization
+			data.is_KC = mode.c == 'OpArgK' and data.C > 0xFF
+		elseif args == 'ABx' then
+			data.Bx = bit.band(bit.rshift(ins, 14), 0x3FFFF)
+			data.is_K = mode.b == 'OpArgK'
+		elseif args == 'AsBx' then
+			data.sBx = bit.band(bit.rshift(ins, 14), 0x3FFFF) - 131071
 		end
-		function get_int32()
-            local a, b, c, d = bytecode:byte(index, index + 3);
-            index = index + 4;
-            return d*16777216 + c*65536 + b*256 + a
-        end
-        function get_int64()
-            local a = get_int32();
-            local b = get_int32();
-            return b*4294967296 + a;
-        end
-		function get_float64()
-			local a = get_int32()
-			local b = get_int32()
-			return (-2*get_bits(b, 32)+1)*(2^(get_bits(b, 21, 31)-1023))*
-			       ((get_bits(b, 1, 20)*(2^32) + a)/(2^52)+1)
-		end
-		function get_string(len)
-			local str;
-            if len then
-	            str = bytecode:sub(index, index + len - 1);
-	            index = index + len;
-            else
-                len = get_size_t();
-	            if len == 0 then return; end
-	            str = bytecode:sub(index, index + len - 1);
-	            index = index + len;
-            end
-            return str;
-        end
+
+		list[i] = data
 	end
 
-	local function decode_chunk()
-		local chunk;
-		local instructions = {};
-		local constants    = {};
-		local prototypes   = {};
-		local debug = {
-			lines = {};
-		};
+	return list
+end
 
-		chunk = {
-			instructions = instructions;
-			constants    = constants;
-			prototypes   = prototypes;
-			debug = debug;
-		};
+local function stm_const_list(S)
+	local len = S:s_int()
+	local list = table.create(len)
 
-		local num;
+	for i = 1, len do
+		local tt = stm_byte(S)
+		local k
 
-		chunk.name       = get_string();-- Function name
-		chunk.first_line = get_int();	-- First line
-		chunk.last_line  = get_int();	-- Last  line
-
-        if chunk.name then chunk.name = chunk.name:sub(1, -2); end
-
-		chunk.upvalues  = get_int8();
-		chunk.arguments = get_int8();
-		chunk.varg      = get_int8();
-		chunk.stack     = get_int8();
-
-        -- TODO: realign lists to 1
-		-- Decode instructions
-		do
-			num = get_int();
-			for i = 1, num do
-				local instruction = {
-					-- opcode = opcode number;
-					-- type   = [ABC, ABx, AsBx]
-					-- A, B, C, Bx, or sBx depending on type
-				};
-
-				local data   = get_int32();
-				local opcode = get_bits(data, 1, 6);
-				local type   = lua_opcode_types[opcode + 1];
-
-				instruction.opcode = opcode;
-				instruction.type   = type;
-
-				instruction.A = get_bits(data, 7, 14);
-				if type == "ABC" then
-					instruction.B = get_bits(data, 24, 32);
-					instruction.C = get_bits(data, 15, 23);
-				elseif type == "ABx" then
-					instruction.Bx = get_bits(data, 15, 32);
-				elseif type == "AsBx" then
-					instruction.sBx = get_bits(data, 15, 32) - 131071;
-				end
-
-				instructions[i] = instruction;
-			end
+		if tt == 1 then
+			k = stm_byte(S) ~= 0
+		elseif tt == 3 then
+			k = S:s_num()
+		elseif tt == 4 then
+			k = stm_lstring(S)
 		end
 
-		-- Decode constants
-		do
-			num = get_int();
-			for i = 1, num do
-				local constant = {
-					-- type = constant type;
-					-- data = constant data;
-				};
-				local type = get_int8();
-				constant.type = type;
-
-				if type == 1 then
-					constant.data = (get_int8() ~= 0);
-				elseif type == 3 then
-					constant.data = get_float64();
-				elseif type == 4 then
-					constant.data = get_string():sub(1, -2);
-				end
-
-				constants[i-1] = constant;
-			end
-		end
-
-		-- Decode Prototypes
-		do
-			num = get_int();
-			for i = 1, num do
-				prototypes[i-1] = decode_chunk();
-			end
-		end
-
-		-- Decode debug info
-        -- Not all of which is used yet.
-		do
-			-- line numbers
-			local data = debug.lines
-			num = get_int();
-			for i = 1, num do
-				data[i] = get_int32();
-			end
-
-			-- locals
-			num = get_int();
-			for i = 1, num do
-				get_string():sub(1, -2);	-- local name
-				get_int32();	-- local start PC
-				get_int32();	-- local end   PC
-			end
-
-			-- upvalues
-			num = get_int();
-			for i = 1, num do
-				get_string();	-- upvalue name
-			end
-		end
-
-		return chunk;
+		list[i] = k -- offset +1 during instruction decode
 	end
 
-	-- Verify bytecode header
-	do
-		assert(get_string(4) == "\27Lua", "Lua bytecode expected.");
-		assert(get_int8() == 0x51, "Only Lua 5.1 is supported.");
-		get_int8(); 	-- Oficial bytecode
-		big_endian = (get_int8() == 0);
-        int_size = get_int8();
-        size_t   = get_int8();
+	return list
+end
 
-        if int_size == 4 then
-            get_int = get_int32;
-        elseif int_size == 8 then
-            get_int = get_int64;
-        else
-	        -- TODO: refactor errors into table
-            error("Unsupported bytecode target platform");
-        end
+local function stm_sub_list(S, src)
+	local len = S:s_int()
+	local list = table.create(len)
 
-        if size_t == 4 then
-            get_size_t = get_int32;
-        elseif size_t == 8 then
-            get_size_t = get_int64;
-        else
-            error("Unsupported bytecode target platform");
-        end
-
-        assert(get_string(3) == "\4\8\0",
-	           "Unsupported bytecode target platform");
+	for i = 1, len do
+		list[i] = stm_lua_func(S, src) -- offset +1 in CLOSURE
 	end
 
-	return decode_chunk();
+	return list
+end
+
+local function stm_line_list(S)
+	local len = S:s_int()
+	local list = table.create(len)
+
+	for i = 1, len do list[i] = S:s_int() end
+
+	return list
+end
+
+local function stm_loc_list(S)
+	local len = S:s_int()
+	local list = table.create(len)
+
+	for i = 1, len do list[i] = {varname = stm_lstring(S), startpc = S:s_int(), endpc = S:s_int()} end
+
+	return list
+end
+
+local function stm_upval_list(S)
+	local len = S:s_int()
+	local list = table.create(len)
+
+	for i = 1, len do list[i] = stm_lstring(S) end
+
+	return list
+end
+
+function stm_lua_func(S, psrc)
+	local proto = {}
+	local src = stm_lstring(S) or psrc -- source is propagated
+
+	proto.source = src -- source name
+
+	S:s_int() -- line defined
+	S:s_int() -- last line defined
+
+	proto.num_upval = stm_byte(S) -- num upvalues
+	proto.num_param = stm_byte(S) -- num params
+
+	stm_byte(S) -- vararg flag
+	proto.max_stack = stm_byte(S) -- max stack size
+
+	proto.code = stm_inst_list(S)
+	proto.const = stm_const_list(S)
+	proto.subs = stm_sub_list(S, src)
+	proto.lines = stm_line_list(S)
+
+	stm_loc_list(S)
+	stm_upval_list(S)
+
+	-- post process optimization
+	for _, v in ipairs(proto.code) do
+		if v.is_K then
+			v.const = proto.const[v.Bx + 1] -- offset for 1 based index
+		else
+			if v.is_KB then v.const_B = proto.const[v.B - 0xFF] end
+
+			if v.is_KC then v.const_C = proto.const[v.C - 0xFF] end
+		end
+	end
+
+	return proto
+end
+
+function decode_bytecode(src)
+	-- func reader
+	local rdr_func
+
+	-- header flags
+	local little
+	local size_int
+	local size_szt
+	local size_ins
+	local size_num
+	local flag_int
+
+	-- stream object
+	local stream = {
+		-- data
+		index = 1,
+		source = src,
+	}
+
+	assert(stm_string(stream, 4) == '\27Lua', 'invalid Lua signature')
+	assert(stm_byte(stream) == 0x51, 'invalid Lua version')
+	assert(stm_byte(stream) == 0, 'invalid Lua format')
+
+	little = stm_byte(stream) ~= 0
+	size_int = stm_byte(stream)
+	size_szt = stm_byte(stream)
+	size_ins = stm_byte(stream)
+	size_num = stm_byte(stream)
+	flag_int = stm_byte(stream) ~= 0
+
+	rdr_func = little and rd_int_le or rd_int_be
+	stream.s_int = cst_int_rdr(size_int, rdr_func)
+	stream.s_szt = cst_int_rdr(size_szt, rdr_func)
+	stream.s_ins = cst_int_rdr(size_ins, rdr_func)
+
+	if flag_int then
+		stream.s_num = cst_int_rdr(size_num, rdr_func)
+	elseif float_types[size_num] then
+		stream.s_num = cst_flt_rdr(size_num, float_types[size_num][little and 'little' or 'big'])
+	else
+		error('unsupported float size')
+	end
+
+	return stm_lua_func(stream, '@virtual')
 end
